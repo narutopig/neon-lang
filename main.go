c99cbda879c76fea090aca9486614e1cac3b762b
pogtato <69479556+narutopig@users.noreply.github.com>
2022-11-01 11:28:49 -0700

    no clue

--- main.go
+++ main.go
@@ -5,29 +5,10 @@ import (
 	"log"
 	"os"
 
-	"github.com/narutopig/neon-lang/lang/expressions"
 	"github.com/narutopig/neon-lang/lang/lexer"
-	t "github.com/narutopig/neon-lang/lang/token"
 )
 
 func main() {
-	ts, ne := expressions.Shunt([]t.Token{
-		t.New(t.NUMVALUE, "4"),
-		t.New(t.ADD, ""),
-		t.New(t.NUMVALUE, "18"),
-		t.New(t.DIVIDE, ""),
-		t.New(t.LEFTPAREN, ""),
-		t.New(t.NUMVALUE, "9"),
-		t.New(t.SUBTRACT, ""),
-		t.New(t.NUMVALUE, "3"),
-		t.New(t.RIGHTPAREN, ""),
-	},
-		1)
-
-	val, ne := expressions.Eval(ts)
-
-	fmt.Println(val)
-
 	// cli entry point
 	if len(os.Args) < 2 {
 		log.Panic("No input file found")
